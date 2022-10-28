94ac8a7493f6d9ce82b414cd24b720759b294d52
gco1on <colon.gianna03@gmail.com>
2022-10-27 22:08:28 -0400

    Created classes

--- Project1.java
+++ Project1.java
@@ -1,13 +1,41 @@
+/*
+- Project 1
+- Daniel Gonzalez, Gianna Colon, and cealer FIXME
+*/
+
 public class Project1 {
     public static void main(String[] args) {
         // Test code goes here
     }
 }
 //-------------------------------------------------
-class Faculty {
+abstract class Person {
+
+    // Print method
+    public abstract void print();
+}
+//-------------------------------------------------
+class Student extends Person {
 
 }
 //-------------------------------------------------
-class Student {
+abstract class Employee extends Person {
 
+}
+//-------------------------------------------------
+class Faculty extends Employee {
+
+}
+//-------------------------------------------------
+class Staff extends Employee {
+
+}
+//-------------------------------------------------
+class Personnel {  // private field = array of type Person
+    private Person[] list;
+
+    // Constructor
+    public Personnel() {
+        list = new Person[100];
+    }
 }
