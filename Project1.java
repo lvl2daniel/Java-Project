4d350dd337f62ad5e547b3fea243f28365bf16b6
Daniel Gonzalez <Danielgonzalez4444@yahoo.com>
2022-10-27 22:27:56 -0400

    Make Variable Names

--- Project1.java
+++ Project1.java
@@ -17,18 +17,58 @@ abstract class Person {
 //-------------------------------------------------
 class Student extends Person {
 
+    @Override
+    public void print() {
+        
+    }
+    
 }
 //-------------------------------------------------
 abstract class Employee extends Person {
+    protected String department;
 
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
 //-------------------------------------------------
 class Faculty extends Employee {
 
+    private String rank;
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
 //-------------------------------------------------
 class Staff extends Employee {
+    private String status;
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public void print() {
+        
+    }
 }
 //-------------------------------------------------
 class Personnel {
