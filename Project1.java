f6bf1d4d89b34747c229b4ca8c8208f53ad42b62
gco1on <colon.gianna03@gmail.com>
2022-10-27 21:50:52 -0400

    Created Project1.java

---
+++ Project1.java
@@ -0,0 +1,13 @@
+public class Project1 {
+    public static void main(String[] args) {
+        // Test code goes here
+    }
+}
+//-------------------------------------------------
+class Faculty {
+
+}
+//-------------------------------------------------
+class Student {
+
+}
