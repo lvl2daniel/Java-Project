e4e4570b13f6903971b95076c40548a06ae457ad
Gianna Colon <82895366+gco1on@users.noreply.github.com>
2022-12-01 01:18:32 -0500

    changed import to java.util.* to be cleaner

--- Project2.java
+++ Project2.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*; // Use * to access all java.util
 
 /*
 - Project 2
